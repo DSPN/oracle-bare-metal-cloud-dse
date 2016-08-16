4c39fce020f7fa34a847ae44baf276054c56f0e2
benofben <ben.lackey@outlook.com>
2016-08-16 16:03:04 -0700

    restructure

---
+++ foo.py
@@ -0,0 +1,69 @@
+class SignedRequestAuth(requests.auth.AuthBase):
+    """A requests auth instance that can be reused across requests"""
+    generic_headers = [
+        "date",
+        "(request-target)"
+    ]
+    body_headers = [
+        "content-length",
+        "content-type",
+        "x-content-sha256",
+    ]
+    required_headers = {
+        "options": [],
+        "get": generic_headers,
+        "head": generic_headers,
+        "delete": generic_headers,
+        "put": generic_headers + body_headers,
+        "post": generic_headers + body_headers
+    }
+
+    def __init__(self, key_id, private_key):
+        # Build a httpsig.requests_auth.HTTPSignatureAuth for each
+        # HTTP method's required headers
+        self.signers = {}
+        for method, headers in six.iteritems(self.required_headers):
+            signer = httpsig.sign.HeaderSigner(
+                key_id=key_id, secret=private_key,
+                algorithm="rsa-sha256", headers=headers[:])
+            use_host = "host" in headers
+            self.signers[method] = (signer, use_host)
+
+    def inject_missing_headers(self, request, sign_body):
+        # Inject date and content-type if missing
+        request.headers.setdefault(
+            "date", email.utils.formatdate(usegmt=True))
+        request.headers.setdefault("content-type", "application/json")
+
+        # Requests with a body need to send content-type,
+        # content-length, and x-content-sha256
+        if sign_body:
+            body = request.body or ""
+            if "x-content-sha256" not in request.headers:
+                m = hashlib.sha256(body.encode("utf-8"))
+                base64digest = base64.b64encode(m.digest())
+                base64string = base64digest.decode("utf-8")
+                request.headers["x-content-sha256"] = base64string
+            request.headers.setdefault("content-length", len(body))
+
+    def __call__(self, request):
+        verb = request.method.lower()
+        signer, use_host = self.signers.get(verb, (None, None))
+        if signer is None:
+            raise ValueError(
+                "Don't know how to sign request verb {}".format(verb))
+
+        # Inject body headers for put/post requests, date for all requests
+        sign_body = verb in ["put", "post"]
+        self.inject_missing_headers(request, sign_body=sign_body)
+
+        if use_host:
+            host = six.moves.urllib.parse.urlparse(request.url).netloc
+        else:
+            host = None
+
+        signed_headers = signer.sign(
+            request.headers, host=host,
+            method=request.method, path=request.path_url)
+        request.headers.update(signed_headers)
+        return request
