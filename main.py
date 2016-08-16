6889924b86d861c517909de9c37e1da55924da73
benofben <ben.lackey@outlook.com>
2016-08-16 16:15:25 -0700

    ready to test

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
   nodesPerAvailabilityDomain = clusterParameters['nodesPerAvailabilityDomain']
   shape = clusterParameters['shape']
   userOCID = clusterParameters['userOCID']
-  tenancyOCID = clusterParameters['tenancyOCID']
+  compartmentId = clusterParameters['compartmentId']
   apiKeyFingerprint = clusterParameters['apiKeyFingerprint']
   privateKeyPath = clusterParameters['privateKeyPath']
 
@@ -23,36 +23,18 @@ def main():
 
   # This is the keyId for a key uploaded through the console
   api_key = (tenancyOCID, userOCID, apiKeyFingerprint)
-
   auth = SignedRequestAuth(api_key, private_key)
 
   headers = {
     "content-type": "application/json",
-    # Uncomment to use a fixed date
-    # "date": "Thu, 05 Jan 2014 21:31:40 GMT"
     "date": email.utils.formatdate(usegmt=True)
   }
 
-  # GET with query parameters
-  uri = "https://core.us-az-phoenix-1.oracleiaas.com/v1/instances?availabilityDomain={availability_domain}&compartmentId={compartment_id}&displayName={display_name}&volumeId={volume_id}"
-  uri = uri.format(
-    availability_domain="Pjwf%3A%20PHX-AD-1",
-    # Older ocid formats included ":" which must be escaped
-    compartment_id="ocid1.compartment.oc1..aaaaaaaayzim47sto5wqh5d4vugrsx566gjqmflvhlifte3p5ez3miy6e4lq".replace(":", "%3A"),
-    display_name="TeamXInstances",
-    volume_id="ocid1.volume.oc1.phx.abyhqljrav2k323acohquoxszz2zyh5vj5v2gbvntg7ifd4ndusyvr332whq".replace(":", "%3A")
-  )
-  response = requests.get(uri, auth=auth, headers=headers)
-  print(uri)
-  print(response.request.headers["Authorization"])
+  uri = ("https://core.us-az-phoenix-1.oracleiaas.com/v1/shapes/?",
+         "availabilityDomain=" + availabilityDomains[0],
+         "&compartmentId=" + compartmentId)
 
-  # POST with body
-  uri = "https://core.us-az-phoenix-1.oracleiaas.com/v1/volumeAttachments"
-  body = """{
-    "compartmentId": "ocid1.compartment.oc1..aaaaaaaayzim47sto5wqh5d4vugrsx566gjqmflvhlifte3p5ez3miy6e4lq",
-    "instanceId": "ocid1.instance.oc1.phx.abuw4ljrlsfiqw6vzzxb43vyypt4pkodawglp3wqxjqofakrwvou52gb6s5a",
-    "volumeId": "ocid1.volume.oc1.phx.abyhqljrav2k323acohquoxszz2zyh5vj5v2gbvntg7ifd4ndusyvr332whq"
-  }"""
-  response = requests.post(uri, auth=auth, headers=headers, data=body)
-  print("\n" + uri)
-  print(response.request.headers["Authorization"])
+  response = requests.get(uri, auth=auth, headers=headers)
+
+  print("uri: " + uri)
+  print("response: " + response.text)
