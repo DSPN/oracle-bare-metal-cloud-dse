c67ccbdade2205edb5f5b958e4f25a0f8ba55385
benofben <ben.lackey@outlook.com>
2016-08-16 16:22:37 -0700

    ...closer

--- main.py
+++ main.py
@@ -22,19 +22,20 @@ def main():
     private_key = f.read().strip()
 
   # This is the keyId for a key uploaded through the console
-  api_key = (tenancyOCID, userOCID, apiKeyFingerprint)
-  auth = SignedRequestAuth(api_key, private_key)
+  api_key = (compartmentId, userOCID, apiKeyFingerprint)
+  auth = SignedRequestAuth.SignedRequestAuth(api_key, private_key)
 
   headers = {
     "content-type": "application/json",
     "date": email.utils.formatdate(usegmt=True)
   }
 
-  uri = ("https://core.us-az-phoenix-1.oracleiaas.com/v1/shapes/?",
-         "availabilityDomain=" + availabilityDomains[0],
+  uri = ("https://core.us-az-phoenix-1.oracleiaas.com/v1/shapes/?" +
+         "availabilityDomain=" + availabilityDomains[0][0] +
          "&compartmentId=" + compartmentId)
+  print("uri: " + uri)
 
   response = requests.get(uri, auth=auth, headers=headers)
-
-  print("uri: " + uri)
   print("response: " + response.text)
+
+main()
