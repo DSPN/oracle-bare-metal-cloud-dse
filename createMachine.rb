d253a047c9e5479db2c87d48ebc17f0d76d781cf
benofben <ben.lackey@outlook.com>
2016-09-13 16:41:35 -0700

    multi machine loop

--- createMachine.rb
+++ createMachine.rb
@@ -1,26 +1,27 @@
 require 'oraclebmc'
 
-subnet_id1 = 'ocid1.subnet.oc1.phx.aaaaaaaaf6kowbejussojcfenl5djacznsdwaivxfqzskadvbjs7m7534qxa'
-subnet_id2 = 'ocid1.subnet.oc1.phx.aaaaaaaa4wvkz5cuz37iafelqsqf7aqx3zlxc52jkudcxiaxsgtgif5ytsga'
-subnet_id3 = 'ocid1.subnet.oc1.phx.aaaaaaaayezdgxnv4tlp7dt55cl3txtxkvtkbf63shhtwx5loulxflgflgja'
-
+subnet_id = ['ocid1.subnet.oc1.phx.aaaaaaaaf6kowbejussojcfenl5djacznsdwaivxfqzskadvbjs7m7534qxa','ocid1.subnet.oc1.phx.aaaaaaaa4wvkz5cuz37iafelqsqf7aqx3zlxc52jkudcxiaxsgtgif5ytsga','ocid1.subnet.oc1.phx.aaaaaaaayezdgxnv4tlp7dt55cl3txtxkvtkbf63shhtwx5loulxflgflgja']
 compartment_id = 'ocid1.tenancy.oc1..aaaaaaaaiecpb6fwi33blxe7x7s4btruzrzj77j2javhie3xevuifa2e7fnq'
-
-availability_domain1 = 'FcAL:PHX-AD-1'
-availability_domain2 = 'FcAL:PHX-AD-2'
-availability_domain3 = 'FcAL:PHX-AD-3'
+availability_domain = ['FcAL:PHX-AD-1','FcAL:PHX-AD-2','FcAL:PHX-AD-3']
 
 ssh_public_key = File.open(File.expand_path('/Users/ben/.ssh/id_rsa.pub'), "rb").read
 
-request = OracleBMC::LaunchInstanceDetails.new
-request.availability_domain = availability_domain1
-request.compartment_id = compartment_id
-request.display_name = 'my_instance'
-request.image_id = 'ol7.2-base-0.0.2'
-request.shape = 'BM.Standard1.36'
-request.subnet_id = subnet_id1
-request.metadata = {'ssh_authorized_keys' => ssh_public_key}
+$i = 0
+while $i < 3  do
+   puts("Deploying machine number #$i" )
 
-api = OracleBMC::ComputeApi.new
-response = api.launch_instance(request)
-response = api.get_instance(response.data.id).wait_until(:lifecycle_state, OracleBMC::Instance::LIFECYCLE_STATE_RUNNING, max_wait_seconds:300)
+#   request = OracleBMC::LaunchInstanceDetails.new
+#   request.availability_domain = availability_domain[$i]
+#   request.compartment_id = compartment_id
+#   request.display_name = 'my_instance'
+#   request.image_id = 'ol7.2-base-0.0.2'
+#   request.shape = 'BM.Standard1.36'
+#   request.subnet_id = subnet_id[$i]
+#   request.metadata = {'ssh_authorized_keys' => ssh_public_key}
+
+#   api = OracleBMC::ComputeApi.new
+#   response = api.launch_instance(request)
+#   response = api.get_instance(response.data.id).wait_until(:lifecycle_state, OracleBMC::Instance::LIFECYCLE_STATE_RUNNING, max_wait_seconds:300)
+
+   $i +=1
+end
