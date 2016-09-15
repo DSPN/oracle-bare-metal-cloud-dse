0c736938556bc18c817159e0ba6eb363e83fa0e3
benofben <ben.lackey@outlook.com>
2016-09-14 17:40:05 -0700

    closer to mount

--- dse.sh
+++ dse.sh
@@ -2,13 +2,21 @@
 
 # This script needs to be run on each node that will run DSE
 
-# Turn off the firewall
+##### Turn off the firewall
 sudo service firewalld stop
 sudo chkconfig firewalld off
 
-# Mount disks
-sudo pvcreate /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1
-sudo vgcreate vg-nvme /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n /dev/nvme3n1
+##### Mount disks
 
-sudo lvcreate --name lv --size 12.8T vg-nvme
+# Install LVM software:
+sudo yum -y install lvm2 dmsetup mdadm reiserfsprogs xfsprogs
+
+# Create disk partitions for LVM:
+sudo pvcreate /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1
+
+# Create volume group upon disk partitions:
+sudo vgcreate vg-nvme /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1
+
+sudo lvcreate --name lv --size 11.6T vg-nvme
+sudo mkdir /cassandra
 sudo mount /dev/vg-nvme/lv /cassandra
