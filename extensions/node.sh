14f7d90f0809809b58146d50482b37de4e7f9e96
Gilbert Lau <gmflau@users.noreply.github.com>
2016-10-15 16:27:44 -0700

    Changed mount /dev/vg-nvme/lv /mnt/data1
    changed from /cassandra to /mnt/data1

--- extensions/node.sh
+++ extensions/node.sh
@@ -1,45 +1,30 @@
 #!/usr/bin/env bash
 
-
-# Retrieve argument(s)
-# First argument contains seed_plus_opscenter_node_ip if provided
-
-
-# This script needs to be run on each node that will run DSE
+##### First argument contains seed_plus_opscenter_node_ip if provided
 
 ##### Turn off the firewall
 
 service firewalld stop
-
 chkconfig firewalld off
 
 ##### Mount disks
 
 # Install LVM software:
-
 yum -y install lvm2 dmsetup mdadm reiserfsprogs xfsprogs
 
 # Create disk partitions for LVM:
-
 pvcreate /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1
 
 # Create volume group upon disk partitions:
-
 vgcreate vg-nvme /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1
-
 lvcreate --name lv --size 11.6T vg-nvme
-
 mkfs.ext4 /dev/vg-nvme/lv
-
-mkdir /cassandra
-
-mount /dev/vg-nvme/lv /cassandra
-
-chmod 777 /cassandra
+mkdir /mnt/data1
+mount /dev/vg-nvme/lv /mnt/data1
+chmod 777 /mnt/data1
 
 
-
-# Installing DataStax Node
+##### Installing DataStax Node
 
 # Retrieve local IP address
 local_ip=
@@ -55,7 +40,7 @@ yum -y install unzip
 unzip master.zip
 cd install-datastax-redhat-master/bin
 
-# if seed_plus_opscenter_node_ip is not defined then pass localip, else pass seed_plus_opscenter_node_ip
+# if seed_plus_opscenter_node_ip is not defined then pass local_ip, else pass seed_plus_opscenter_node_ip
 if [ -z "$1" ]
   then
     ./dse.sh bmc $local_ip us-phoenix-1 $local_ip
