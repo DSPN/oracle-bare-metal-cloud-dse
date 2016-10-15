bd69598e4d01aa7ace8d4c98240e09e4eae6cefb
Gilbert Lau <gmflau@gmail.com>
2016-10-15 16:36:16 -0700

    Updated for Ruby SDK 0.8.0

--- test.rb
+++ test.rb
@@ -3,6 +3,6 @@ require 'oraclebmc'
 # This will load the config file at the default location, and will
 # use the tenancy from that config as the compartment in the
 # call to list_users.
-api = OracleBMC::IdentityApi.new
+api = OracleBMC::Identity::IdentityClient.new(region: OracleBMC::REGION_US_PHOENIX_1)
 response = api.list_users(OracleBMC.config.tenancy)
 response.data.each { |user| puts user.name }
